8d3597bfdce1483eceed0301261e52154379c3ab
Zoltan Nagy <abesto@abesto.net>
2013-10-28 23:53:05 +0100

    Highlight push action when p or push is in command line

--- public/source/commandLine.js
+++ public/source/commandLine.js
@@ -1,4 +1,4 @@
-
+// Big todo: graph node hasFocus vs. commandLine hasFocus
 var ko = require('../vendor/js/knockout-2.2.1');
 var $ = require('../vendor/js/jquery-2.0.0.min');
 
@@ -16,15 +16,19 @@ var CommandLineViewModel = function(path) {
     this.hasFocus = ko.observable(true);
     this.command = ko.observable();
     $(document).keypress(function() { self.hasFocus(true); });
+    this.command.subscribe(this.show, this);
 }
 exports.CommandLineViewModel = CommandLineViewModel;
 CommandLineViewModel.prototype.template = 'commandLine';
-CommandLineViewModel.prototype.call = function() {
-    var command = this.command();
-    this.command('');
+CommandLineViewModel.prototype.applyAliases = function(command) {
     while (aliases[command]) {
         command = aliases[command];
     }
+    return command;
+}
+CommandLineViewModel.prototype.call = function() {
+    var command = this.applyAliases(this.command());
+    this.command('');
     if (this[command]) return this[command]();
 }
 CommandLineViewModel.prototype.commit = function() {
@@ -37,4 +41,20 @@ CommandLineViewModel.prototype.push = function() {
     }
     // TODO: get the push actions using filter
     this.path.repository().graph.checkedOutRef().node().dropareaGraphActions[3].doPerform()
+}
+CommandLineViewModel.prototype.show = function(command) {
+    var graph = this.path.repository().graph;
+    if (!graph.currentActionContext()) {
+        graph.checkedOutRef().hasFocus(true);
+    }
+    command = this.applyAliases(this.command());
+    if (this.show[command]) {
+        this.show[command].call(this);   
+    } else {
+        graph.hoverGraphAction(null);
+    }
+}
+CommandLineViewModel.prototype.show.push = function() {
+    var graph = this.path.repository().graph;
+    graph.hoverGraphAction(graph.currentActionContext().node().dropareaGraphActions[3]);
 }
