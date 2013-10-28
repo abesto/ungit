f4668e87722d59779bf371af3040423b58825522
Zoltan Nagy <abesto@abesto.net>
2013-10-28 23:36:46 +0100

    Add push action

--- public/source/commandLine.js
+++ public/source/commandLine.js
@@ -5,7 +5,8 @@ var $ = require('../vendor/js/jquery-2.0.0.min');
 
 // Will be loaded from config
 var aliases = {
-    'c': 'commit'
+    'c': 'commit',
+    'p': 'push'
 }
 
 
@@ -28,4 +29,12 @@ CommandLineViewModel.prototype.call = function() {
 }
 CommandLineViewModel.prototype.commit = function() {
     this.path.repository().staging.commitMessageTitleFocused(true);
+}
+CommandLineViewModel.prototype.push = function() {
+    var graph = this.path.repository().graph;
+    if (!graph.currentActionContext()) {
+        graph.checkedOutRef().hasFocus(true);
+    }
+    // TODO: get the push actions using filter
+    this.path.repository().graph.checkedOutRef().node().dropareaGraphActions[3].doPerform()
 }
