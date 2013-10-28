9964079b716396633d2f4d585fced7e630290c24
Zoltan Nagy <abesto@abesto.net>
2013-10-28 22:58:28 +0100

    Alias: c -> commit

--- public/source/commandLine.js
+++ public/source/commandLine.js
@@ -2,6 +2,12 @@
 var ko = require('../vendor/js/knockout-2.2.1');
 
 
+// Will be loaded from config
+var aliases = {
+    'c': 'commit'
+}
+
+
 var CommandLineViewModel = function(path) {
     var self = this;
     this.path = path;
@@ -13,7 +19,10 @@ CommandLineViewModel.prototype.template = 'commandLine';
 CommandLineViewModel.prototype.call = function() {
     var command = this.command();
     this.command('');
-    return this[command]();
+    while (aliases[command]) {
+        command = aliases[command];
+    }
+    if (this[command]) return this[command]();
 }
 CommandLineViewModel.prototype.commit = function() {
     this.path.repository().staging.commitMessageTitleFocused(true);
